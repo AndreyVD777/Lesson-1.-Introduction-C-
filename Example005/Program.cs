a19a4f8e6c3b31b99cf5bc37403c28a45accbed7
AndreyVD <vdovinandrei@gmail.com>
2022-11-11 19:35:22 +0300

    add masha

---
+++ Example005/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите ваше имя");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+
+{
+    Console.WriteLine("Ура, да это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine("username");
+}
