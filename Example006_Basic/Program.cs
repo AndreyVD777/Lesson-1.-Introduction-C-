32623d502c64375f9bbca0de18abb668dfdc2e31
AndreyVD <vdovinandrei@gmail.com>
2022-11-12 08:35:08 +0300

    нахождение макс числа

--- Example006_Basic/Program.cs
+++ Example006_Basic/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (max < a) max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+
+Console.Write("max ");
+Console.WriteLine(max);
+
+
+
